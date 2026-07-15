47ffe78485e4f8a3552e50d391cb7f2de11fbc03
Mio Ngaolakorn <ngaolakhorny@gmail.com>
2026-07-15 11:37:28 +0700

    fix: download run.ps1 from GitHub automatically

--- Install_New_theme.bat
+++ Install_New_theme.bat
@@ -3,7 +3,7 @@
 :: Line for changing spotx parameters, each parameter should be separated by a space
 set param=-new_theme -no_pause
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command "& '%~dp0run.ps1' %param%"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -Command "iex \"& { $(iwr -useb 'https://raw.githubusercontent.com/ynmio55/spoti/main/run.ps1') } %param%\""
 
 pause
 exit /b
